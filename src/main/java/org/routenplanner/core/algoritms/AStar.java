a9fc464d6511e918e10e2bbe1927bb73acd611b8
Jens Wilberg <jens_wilberg@outlook.com>
2018-01-09 12:11:29 +0100

    AStar init
    Erste ideen für Kanten und Knoten

---
+++ src/main/java/org/routenplanner/core/algoritms/AStar.java
@@ -0,0 +1,9 @@
+package org.routenplanner.core.algoritms;
+
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+
+public class AStar {
+
+    public void computeHeuristic(){}
+}
---
+++ src/main/java/org/routenplanner/core/graph/Edge.java
@@ -0,0 +1,13 @@
+package org.routenplanner.core.graph;
+
+public class Edge {
+    private float g;
+    private float h;
+    private float f;
+    // Start konten
+    // end knoten
+
+    public Edge(float g) {
+        this.g = g;
+    }
+}
