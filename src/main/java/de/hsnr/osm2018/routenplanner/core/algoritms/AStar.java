d23cbb7caf285a8e7506971e693d53d120c2d42b
Jens Wilberg <jens_wilberg@outlook.com>
2018-01-10 15:19:32 +0100

    Vorläufiger AStern

--- src/main/java/de/hsnr/osm2018/routenplanner/core/algoritms/AStar.java
+++ src/main/java/de/hsnr/osm2018/routenplanner/core/algoritms/AStar.java
@@ -1,9 +1,50 @@
 package de.hsnr.osm2018.routenplanner.core.algoritms;
 
+import de.hsnr.osm2018.routenplanner.core.graph.*;
 import java.util.PriorityQueue;
 import java.util.ArrayList;
 
 public class AStar {
 
-    public void computeHeuristic(){}
+    public boolean voidrunAStar(Node root, Node goal){
+        PriorityQueue<Node> openlist = new PriorityQueue<>();
+        ArrayList<Node> closedlist = new ArrayList<>();
+
+        openlist.add(root);
+
+        while(!openlist.isEmpty()){
+            Node u = openlist.poll();
+            if(u == goal){ // TODO überprüfen, ob compare equal überschreibt
+                return true;
+            }
+            if(!closedlist.contains(u)) {
+                closedlist.add(u);
+                for (Edge e : u.getEdges()) {
+                    Node neighbour = e.getEnd();
+                    float dist = u.getWeight() + e.getWeight();
+
+                    if(openlist.contains(neighbour) && neighbour.getWeight() > dist){
+                        neighbour.setWeight(dist);
+                        neighbour.computeHeuristic(goal);
+                        neighbour.setParent(u);
+                    } else if(neighbour.getParent() == null){
+                        neighbour.setWeight(dist);
+                        neighbour.computeHeuristic(goal);
+                        neighbour.setParent(u);
+                        openlist.add(neighbour);
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Node> getPath(Node goal){
+        ArrayList<Node> = new ArrayList<>();
+        Node current = goal;
+        while(current.getParent() != null){
+
+        }
+    }
 }
--- src/main/java/de/hsnr/osm2018/routenplanner/core/graph/Node.java
+++ src/main/java/de/hsnr/osm2018/routenplanner/core/graph/Node.java
@@ -3,19 +3,32 @@ package de.hsnr.osm2018.routenplanner.core.graph;
 import de.hsnr.osm2018.routenplanner.core.graph.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
-public class Node {
+public class Node implements Comparator{
     private int id;
     private float lon;
     private float lat;
-    private float w; // Weight
-    private float h; // Heuristik
+    private float g; // Weight
+    private float f;
+    private Node parent;
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getParent() {
+
+        return parent;
+    }
+
     private ArrayList<Edge> edges; // List of edges from this Node
 
     public Node(int id, float lon, float lat) {
         this.id = id;
         this.lon = lon;
         this.lat = lat;
+        this.g = 0;
     }
 
     public Node(int id, float lon, float lat, ArrayList<Edge> edges) {
@@ -42,7 +55,10 @@ public class Node {
     }
 
     public float getWeight() {
-        return w;
+        return g;
+    }
+    public void setWeight(float weight){
+        this.g = weight;
     }
 
     public ArrayList<Edge> getEdges() {
@@ -52,9 +68,30 @@ public class Node {
     public Edge getMinEdge(){
         return Collections.min(this.edges);
     }
-
-    public void computeWeight(Node goal, Edge e){
-        this.h = Math.abs(goal.lon - this.lon) + Math.abs(goal.lat - this.lat);
-        this.w = this.h + e.getWeight();
+    public float getF(){
+        return f;
     }
+
+    public void computeHeuristic(Node goal){
+        float h = Math.abs(goal.lon - this.lon) + Math.abs(goal.lat - this.lat);
+        this.f = this.g + h;
+    }
+
+    @Override
+    public int compare(Object node1, Object node2) {
+        Node o = (Node) node1;
+        Node t1 = (Node) node2;
+        if(o.getF() > t1.getF()){
+            return 1;
+        }
+
+        else if (o.getF() < t1.getF()){
+            return -1;
+        }
+
+        else{
+            return 0;
+        }
+    }
+
 }
