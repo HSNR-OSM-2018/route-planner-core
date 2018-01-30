2053a25889501322a70edfe8b4620a873d65d43d
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-30 14:21:32 +0100

    Added Implementation for AStar that performs like Dijkstra

---
+++ src/main/java/de/hsnr/osm2018/core/algoritms/DijkstraAStar.java
@@ -0,0 +1,23 @@
+package de.hsnr.osm2018.core.algoritms;
+
+import de.hsnr.osm2018.data.graph.Edge;
+import de.hsnr.osm2018.data.graph.Graph;
+import de.hsnr.osm2018.data.graph.Node;
+import de.hsnr.osm2018.data.graph.NodeContainer;
+
+public class DijkstraAStar extends AStar {
+
+    public DijkstraAStar(Graph graph) {
+        super(graph);
+    }
+
+    @Override
+    public double computeHeuristic(Node start, Node destination) {
+        return 0;
+    }
+
+    @Override
+    public double getDistance(NodeContainer node, Edge edge) {
+        return 0;
+    }
+}
